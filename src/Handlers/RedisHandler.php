4485c97d7855afd686d8a64abb6f3f911e8ca439
Robert Lester <rlester@tcsvc.com>
2024-04-15 09:12:01 -0400

    Update RedisHandler.php

--- src/Handlers/RedisHandler.php
+++ src/Handlers/RedisHandler.php
@@ -28,7 +28,6 @@ class RedisHandler extends CallManager implements CallForwardingDriver
 
         return collect(array_map(function ($member) use ($key, $purge) {
             if ($purge) Redis::connection($this->connection)->srem($key, $member);
-            echo $key;
             return json_decode($member, true);
         }, $members));
     }
